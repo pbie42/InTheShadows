09d24c9d9d46341d675e1ceade1709fbc4f78079
Paul BIE <pbie@e3r12p7.42.fr>
2019-06-07 12:14:24 +0200

    [Wip] - Main Menu Spotlight

---
+++ Assets/Scripts/MainSpotlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSpotlight : MonoBehaviour
+{
+  private float _rotX = 0.0f;
+  private float _rotY = 0.0f;
+  private float _rotZ = 0.0f;
+  private bool _increase = true;
+
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Debug.Log("transform.eulerAngles.x: " + transform.localEulerAngles.x);
+    if (transform.localEulerAngles.x <= -7.0f)
+      _increase = true;
+    else if (transform.localEulerAngles.x >= 7.0f)
+      _increase = false;
+
+    if (_increase)
+      _rotX += 0.1f;
+    else
+      _rotX -= 0.1f;
+    transform.rotation = Quaternion.Euler(_rotX * Time.deltaTime, 90, 0);
+  }
+}
