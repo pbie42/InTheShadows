599e95c843defffd146fafc7755597b45a25880a
Paul BIE <pbie@e3r12p7.42.fr>
2019-06-10 16:50:23 +0200

    [Wip] - Showing scene depending on camera location

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
 	public Transform[] views;
+	public GameObject cartLights;
+	public GameObject objectAndScreen;
 	public float transitionSpeed;
+	private Transform _currentView;
+	private int _viewIndex = 0;
 
 	// Use this for initialization
 	void Start()
@@ -14,9 +17,56 @@ public class CameraController : MonoBehaviour
 
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			cartLights.SetActive(false);
+			_currentView = views[0];
+			_viewIndex = 0;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			objectAndScreen.SetActive(false);
+			cartLights.SetActive(true);
+			if (_viewIndex == 2)
+				_currentView = views[2];
+			else
+				_currentView = views[1];
+			_viewIndex = 1;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			objectAndScreen.SetActive(true);
+			cartLights.SetActive(false);
+			transform.rotation = views[2].rotation;
+			_currentView = views[3];
+			_viewIndex = 2;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			cartLights.SetActive(false);
+			_currentView = views[4];
+			_viewIndex = 3;
+		}
+	}
 
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		UpdatePosition();
+	}
+
+	private void UpdatePosition()
+	{
+		transform.position = Vector3.Lerp(transform.position, _currentView.position, Time.deltaTime * transitionSpeed);
+
+		Vector3 currentAngle = new Vector3(
+			Mathf.Lerp(transform.rotation.eulerAngles.x, _currentView.transform.eulerAngles.x, Time.deltaTime * transitionSpeed),
+			Mathf.Lerp(transform.rotation.eulerAngles.y, _currentView.transform.eulerAngles.y, Time.deltaTime * transitionSpeed),
+			Mathf.Lerp(transform.rotation.eulerAngles.z, _currentView.transform.eulerAngles.z, Time.deltaTime * transitionSpeed)
+		);
+
+		transform.eulerAngles = currentAngle;
 	}
 }
